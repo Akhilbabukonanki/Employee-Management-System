5f7da9217570d9b499671f5ef2b4183701a28549
Konanki Akhil Babu <akhilbabukonanki888@gmail.com>
2026-07-04 10:45:37 +0530

    Remove  WhiteNoise dependency

--- employee_management/employee_management/settings.py
+++ employee_management/employee_management/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Media Files (Uploaded Profile Photos)
 MEDIA_URL = '/media/'
